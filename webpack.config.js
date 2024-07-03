46d42de750ce3d1cac33e85303a043c8c0a9b464
ItsNik <106100177+Its4Nik@users.noreply.github.com>
2024-07-03 21:28:00 +0200

    Update webpack.config.js
    changed Line 15
    
    "dist" to "build"

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "build"),
         clean: true,
     },
     resolve: {
